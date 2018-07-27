52963482d8a0c481276bc6516853a109ea9149e8
turulomio <turulomio@00448442-4016-436b-b878-3948558349b3>
2018-07-27 16:58:22 +0000

    Added video conversion with ffmpeg

--- doc/ttyrec/launch.sh
+++ doc/ttyrec/launch.sh
@@ -3,3 +3,4 @@
 
 #xterm -hold -bg black -geometry 140x400  -fa monaco -fs 18 -fg white -e "ttyrec -e 'bash demo.sh'; ttygif ttyrecord"
 xterm -hold -bg black -geometry 140x400  -fa monaco -fs 18 -fg white -e "ttyrec -e 'python3 demo.py'; ttygif ttyrecord"
+ffmpeg -i tty.gif -c:v libx264 -pix_fmt yuv420p -movflags +faststart tty.mp4
