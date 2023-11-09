6099d64768ebe925104c72223e01d5b52484e5b3
turulomio <turulomio@yahoo.es>
2023-11-09 07:23:12 +0100

    Test code coverage is now 84%

--- toomanyfiles/tests/test_toomanyfiles.py
+++ toomanyfiles/tests/test_toomanyfiles.py
@@ -1,5 +1,6 @@
 from os import chdir
 from toomanyfiles import toomanyfiles
+from pytest import raises
 
 def test_create_examples():
     toomanyfiles.create_examples()
@@ -10,8 +11,19 @@ def test_create_examples():
     toomanyfiles.toomanyfiles(remove=False)
     toomanyfiles.toomanyfiles(remove=True)
     
-#    chdir("../../toomanyfiles_examples/files_with_different_roots/")
-#    toomanyfiles.toomanyfiles(remove=False)
-#    toomanyfiles.toomanyfiles(remove=True)
+    chdir("../../toomanyfiles_examples/files_with_different_roots/")
+    # This command will have exit code 2
+    with raises(SystemExit) as e:
+        toomanyfiles.toomanyfiles(remove=True)
+    assert e.type == SystemExit
+    assert e.value.code == 1    
+    # This command will have exit code 2
+    with raises(SystemExit) as e:
+        toomanyfiles.toomanyfiles(remove=False)
+    assert e.type == SystemExit
+    assert e.value.code == 1
     chdir("../..")
     toomanyfiles.remove_examples()
+    
+def test_main():
+    toomanyfiles.main(["--pretend"])
