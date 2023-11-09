a6ee699fc0bf9e0ce16ccb3c168a31554ee1ea29
turulomio <turulomio@yahoo.es>
2023-11-09 07:08:48 +0100

    Test code coverage is now 76%

--- toomanyfiles/tests/test_toomanyfiles.py
+++ toomanyfiles/tests/test_toomanyfiles.py
@@ -1,6 +1,17 @@
+from os import chdir
 from toomanyfiles import toomanyfiles
-
 
 def test_create_examples():
     toomanyfiles.create_examples()
+    chdir("toomanyfiles_examples/directories/")
+    toomanyfiles.toomanyfiles(remove=False)
+    toomanyfiles.toomanyfiles(remove=True)
+    chdir("../../toomanyfiles_examples/files/")
+    toomanyfiles.toomanyfiles(remove=False)
+    toomanyfiles.toomanyfiles(remove=True)
+    
+#    chdir("../../toomanyfiles_examples/files_with_different_roots/")
+#    toomanyfiles.toomanyfiles(remove=False)
+#    toomanyfiles.toomanyfiles(remove=True)
+    chdir("../..")
     toomanyfiles.remove_examples()
