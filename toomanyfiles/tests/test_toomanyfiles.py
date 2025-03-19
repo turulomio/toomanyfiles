0d66fd4a65003f80b764c98b5c23854f2c9a8ae9
turulomio <turulomio@yahoo.es>
2025-03-19 13:45:52 +0100

    Added mixed files tests

--- toomanyfiles/tests/test_toomanyfiles.py
+++ toomanyfiles/tests/test_toomanyfiles.py
@@ -71,6 +71,20 @@ def test_mixed_patterns():
         assert path.exists(f"{tempdir}/20250101 Hola.xlsx")
         assert not path.exists(f"{tempdir}/20250102 Hola.doc") 
         assert path.exists(f"{tempdir}/20250201 1000 Hola.xlsx") #Got datetime 20250201
+        assert not path.exists(f"{tempdir}/20250202 Hola.xlsx")             
+        
+def test_mixed_files_and_dirs():
+    with TemporaryDirectory() as tempdir:
+        toomanyfiles.create_file(f"{tempdir}/20250101 Hola.xlsx")
+        toomanyfiles.create_file(f"{tempdir}/20250102/20250102 Hola.doc")
+        toomanyfiles.create_file(f"{tempdir}/20250201/20250201 1000 Hola.xlsx")
+        toomanyfiles.create_file(f"{tempdir}/20250202 Hola.xlsx")
+       
+        toomanyfiles.toomanyfiles(tempdir,  remove=True, time_pattern="%Y%m%d",  too_young_to_delete=0,  file_patterns=[])
+
+        assert path.exists(f"{tempdir}/20250101 Hola.xlsx")
+        assert not path.exists(f"{tempdir}/20250102/20250102 Hola.doc") 
+        assert path.exists(f"{tempdir}/20250201/20250201 1000 Hola.xlsx") #Got datetime 20250201
         assert not path.exists(f"{tempdir}/20250202 Hola.xlsx")
         
 def test_date_pattern_with_filter():
