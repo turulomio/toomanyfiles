3643d7f32efc9925514c545447ac1cadb5d14203
turulomio <turulomio@yahoo.es>
2025-11-05 12:48:09 +0100

    Updated gifs in README using vhs command (#24)
    * Recovered create_example and remove_example
    
    * Preparing video script
    
    * Trying
    
    * Created tape and video script
    
    * Created two gifs

--- toomanyfiles/poethepoet.py
+++ toomanyfiles/poethepoet.py
@@ -1,9 +1,12 @@
 from datetime import date
-from toomanyfiles import __version__
-from os import system, chdir
-from gettext import translation, install
+from toomanyfiles import __version__, toomanyfiles
+from os import system, chdir, path, makedirs
+from shutil import rmtree, which, move
+from datetime import datetime, timedelta
+from colorama import Style
+from gettext import translation
 from importlib.resources import files
-from sys import modules
+from sys import modules, exit
         
 try:
     t=translation('toomanyfiles', files("toomanyfiles") / 'locale')
@@ -23,10 +26,23 @@ def coverage():
 
 
 def video():
-    chdir("doc/ttyrec")
-    system("ttyrecgenerator --output toomanyfiles_howto_es 'python3 howto.py' --lc_all es_ES.UTF-8")
-    system("ttyrecgenerator --output toomanyfiles_howto_en 'python3 howto.py' --lc_all C")
+    # Comprobaciones
+    vhs=which("vhs")
+    if vhs is None: 
+        print(_("vhs tool is needed. Look at https://github.com/charmbracelet/vhs"))
+        exit(1)
+
+    chdir("doc")
+    system(f"{vhs} command.tape")
+    chdir("..")
+
+    create_examples()
+    chdir("toomanyfiles_examples/files")
+    system(f"{vhs} ../../doc/howto.tape")
+    move("howto.gif", "../../doc/howto.gif")
     chdir("../..")
+    remove_examples()
+
 
 def translate():
         system("xgettext -L Python --no-wrap --no-location --from-code='UTF-8' -o toomanyfiles/locale/toomanyfiles.pot toomanyfiles/*.py")
@@ -43,8 +59,7 @@ def release():
     print("  * poe translate")
     print("  * mcedit locale/es.po")
     print("  * poe translate")
-    print("  * mcedit doc/ttyrec/howto.py")
-    print("  * python setup.py video" + ". " + _("If changed restart from first python setup.py doc"))
+    print("  * poe video")
     print("  * git commit -a -m 'toomanyfiles-{0}'".format(__version__))
     print("  * git push")
     print(_("  * Make a new tag in github"))
@@ -53,3 +68,37 @@ def release():
     print(_("  * Upload to portage repository")) 
 
 
+## Creates an example subdirectory and fills it with datetime pattern filenames
+def create_examples():
+    if path.exists('toomanyfiles_examples'):
+        rmtree('toomanyfiles_examples')
+    makedirs("toomanyfiles_examples/files", exist_ok=True)
+    number=100
+    for i in range (number):
+        d=datetime.now()-timedelta(days=i)
+        filename="toomanyfiles_examples/files/{}{:02d}{:02d} {:02d}{:02d} Toomanyfiles example.txt".format(d.year,d.month,d.day,d.hour,d.minute)
+        toomanyfiles.create_file(filename)
+
+    makedirs("toomanyfiles_examples/directories", exist_ok=True)
+    number=100
+    for i in range (number):
+        d=datetime.now()-timedelta(days=i)
+        filename="toomanyfiles_examples/directories/{}{:02d}{:02d} {:02d}{:02d} Directory/Toomanyfiles example.txt".format(d.year,d.month,d.day,d.hour,d.minute)
+        makedirs(path.dirname(filename), exist_ok=True)        
+        toomanyfiles.create_file(filename)
+
+    makedirs("toomanyfiles_examples/files_with_different_roots", exist_ok=True)
+    number=5
+    for i in range (number):
+        d=datetime.now()-timedelta(days=i)
+        filename="toomanyfiles_examples/files_with_different_roots/{}{:02d}{:02d} {:02d}{:02d} Toomanyfiles example {}.txt".format(d.year,d.month,d.day,d.hour,d.minute, i)
+        toomanyfiles.create_file(filename)
+
+    print (Style.BRIGHT + _("Different examples have been created in the directory 'toomanyfiles_examples'"))
+
+def remove_examples():
+    if path.exists('toomanyfiles_examples'):
+        rmtree('toomanyfiles_examples')
+        print (_("'toomanyfiles_examples' directory removed"))
+    else:
+        print (_("I can't remove 'toomanyfiles_examples' directory"))
