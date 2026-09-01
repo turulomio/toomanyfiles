62896cce2bfc20561ee87e7729ad679dcbe871df
turulomio <turulomio@yahoo.es>
2026-09-01 05:29:57 +0200

    Updated version

--- toomanyfiles/__init__.py
+++ toomanyfiles/__init__.py
@@ -1,6 +1,6 @@
 from toomanyfiles import toomanyfiles
 
 from datetime import datetime
-__version__ = '1.2.0'
-__versiondatetime__=datetime(2026, 9, 1, 7, 18)
+__version__ = '1.2.1'
+__versiondatetime__=datetime(2026, 9, 1, 5, 28)
 __versiondate__=__versiondatetime__.date()
